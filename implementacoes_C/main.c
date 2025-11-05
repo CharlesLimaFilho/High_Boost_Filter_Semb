07be90d3b00996e45811bf0c168478576bd6330a
Charles Lima <charles.lima.filho17@gmail.com>
2025-11-05 11:39:15 -0300

    ## Adding comments to high_boost.c

--- implementacoes_C/main.c
+++ implementacoes_C/main.c
@@ -2,7 +2,7 @@
 #include "high_boost.h"
 
 int main() {
-    high_boost("../implementacoes_C/inputs/mona_lisa.ascii_maior.pgm", "../implementacoes_C/outputs/mona_lisa.ascii_maior.pgm", 2.0f);
+    high_boost("../implementacoes_C/inputs/mona_lisa.ascii.pgm", "../implementacoes_C/outputs/teste2.pgm", 2.0f);
 
     return 0;
 }
